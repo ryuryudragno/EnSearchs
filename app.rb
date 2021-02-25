b852760923fcfd37cdcd041724dd1ba45726fe8c
ryuryudragno <ryuno138@gmail.com>
2021-02-25 15:52:25 +0000

    goo辞書からスクレイピング

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ Bundler.require
 require 'sinatra/reloader' if development?
 
 require 'open-uri'
+require 'net/http'
+require 'json'
 require 'nokogiri'
 require 'selenium-webdriver'
 
@@ -30,50 +32,43 @@ get '/' do
     # end
 end
 
-post '/auto' do
+get '/search' do
+    @word = params[:searchWord]#検索語
+    @meanings= []
+    
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
-    # options.add_argument("--disable-gpu")
-    # options.add_argument("--window-size=1280x1696")
-    # options.add_argument("--disable-application-cache")
-    # options.add_argument("--disable-infobars")
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--hide-scrollbars")
-    # options.add_argument("--enable-logging")
-    # options.add_argument("--log-level=0")
-    # options.add_argument("--single-process")
-    # options.add_argument("--ignore-certificate-errors")
-    # options.add_argument("--homedir=/tmp")
     driver = Selenium::WebDriver.for :chrome, options: options
     
-    #Googleにアクセスする
-    # driver.get("https://google.com")
-    driver.navigate.to "https://ejje.weblio.jp/"
+    #goo辞書にアクセスする
+    driver.get("https://dictionary.goo.ne.jp/word/en/#{@word}")
    
     # ターミナルへページタイトルを出力
     puts driver.title
-    sleep 1
-    puts driver.find_element(:css, "h2").text
     
-    #検索テキストボックスの要素をid属性値から取得
-    element = driver.find_element(:id,'searchWord')
-    #検索テキストボックスに"Selenium"を入力し検索を実行
-    element.send_keys('りんご', :enter)
+    #意味を複数とってくる
+    @Meanings = driver.find_elements(:class,"contents-wrap-b")
+    #ここからサーバー側で処理する必要がある
+    @Meanings.each do |meaning|
+        @meanings.push(meaning.text)
+    end
+    # length = @meanings.size
+    # @maji = @meanings[a-2].text
     
-    puts driver.find_element(:class,'content-explanation').text#apple
+    # #検索テキストボックスの要素をid属性値から取得
+    # element = driver.find_element(:id,'searchWord')
+    # #検索テキストボックスに"Selenium"を入力し検索を実行
+    # element.send_keys(@word, :enter)
+    
+    # @result1 = driver.find_element(:class,'content-explanation').text#apple
     
     #スクショ撮る
     # driver.save_screenshot("blog.png")
 
     driver.quit # ブラウザ終了
     
-    redirect "/"
+    erb :index
     
-    # #テキストボックスの要素をname属性から取得
-    # element = driver.find_element(:name, 'q')
-    
-    # #取得したテキストボックスの要素に"test"を入力
-    # element.send_keys "test"
     
     # #実行キーの押下
     # element.submit
