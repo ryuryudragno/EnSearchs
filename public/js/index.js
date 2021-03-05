386390283e5b2af53bbb9cde4391acb6afb875e7
ryuryudragno <ryuno138@gmail.com>
2021-03-05 15:52:39 +0000

    デザイン調整 検索語句の値チェック

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
-console.log(10)
+function check(){//入力チェック欄
+    let str = document.word.searchWord.value//検索語句を取得
+    console.log(str)
+    
+    for(var i=0 ; i<str.length; i++){
+        var code=str.charCodeAt(i);
+        if ((65<=code && code<=90) || (97<=code && code<=122)) {
+            /* 半角英字（a-z,A-Z）の文字コード範囲 */
+            /* 半角スペースも許容 */
+        }else{
+            alert("半角英字以外が含まれています");
+            return false;
+        }
+        
+        return true;
+    }
+}
