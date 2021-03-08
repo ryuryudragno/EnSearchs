26ffbec222c7f1819a9377fc87afe49b5367e160
ryuryudragno <ryuno138@gmail.com>
2021-03-08 13:21:35 +0000

    リリース

--- app.rb
+++ app.rb
@@ -127,6 +127,7 @@ get '/search' do
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     options.add_argument('--window-size=1200x600')
     driver = Selenium::WebDriver.for :chrome, options: options
