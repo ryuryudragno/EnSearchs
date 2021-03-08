f5ac2233b6a6603e08b49765ea0677aed44af95a
ryuryudragno <ryuno138@gmail.com>
2021-03-08 13:11:44 +0000

    リリース

--- app.rb
+++ app.rb
@@ -126,6 +126,9 @@ get '/search' do
     #Selenium起動
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--window-size=1200x600')
     driver = Selenium::WebDriver.for :chrome, options: options
     
     #goo辞書にアクセスする
