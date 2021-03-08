9b0279f6125d45c8c16480adaf8665d6dc98344c
ryuryudragno <ryuno138@gmail.com>
2021-03-08 14:44:29 +0000

    リリース

--- app.rb
+++ app.rb
@@ -126,11 +126,9 @@ get '/search' do
     
     #Selenium起動
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')
-    # options.add_argument('--disable-dev-shm-usage')
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('--window-size=1200x600')
+    options.binary = ENV.fetch(“GOOGLE_CHROME_SHIM”)
+    options.add_argument('headless')
+    options.add_argument('disable-gpu')
     driver = Selenium::WebDriver.for :chrome, options: options
     
     # caps = Selenium::WebDriver::Remote::Capabilities.chrome(binary: "/opt/google/chrome/google-chrome", "chromeOptions" => {args: ["--headless"]})
